85e30c9569401016090a171c72687e4d51f9b8fa
Marie Delori <mariedelori@outlook.com>
2022-03-02 16:58:35 +0100

    updated seed bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,34 +16,17 @@ puts "Marie user created"
 paul = User.create!(first_name: 'Paul', last_name: 'SCHAUSS', email: 'paul@localhost.me', password: '123456')
 puts "Paul user created"
 
-illness_names = [
-"Acne",
-"Bloating",
-"Concentration",
-"Depression",
-"Energy",
-"Growth",
-"Hair loss",
-"Immunity",
-"Insomnia",
-"Pregnancy",
-"Sport",
-"Stress & Anxiety"
-]
-
-illness_names.each do |name|
-  puts "adding #{name} ..."
-  illness = Illness.create!(name: name)
 
   CSV.foreach("data_seed/illness_nutrients.csv", headers: :first_row, header_converters: :symbol) do |row|
+    illness = Illness.find_or_create_by(name: row[:illness_name])
     nutrient = Nutrient.find_or_create_by(name: row[:nutrient_name])
 
-    IllnessNutrient.create!(
+    p IllnessNutrient.create!(
       illness: illness,
       nutrient: nutrient
     )
+
   end
-end
 
 nutrient_data = {
 "Vitamin B12" => 'vitB12.csv',
