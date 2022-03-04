1c9f1233f71b0ea407893a62315d0c002269965c
Marie Delori <mariedelori@outlook.com>
2022-03-04 14:52:28 +0100

    next on foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,9 +4,23 @@ class FoodsController < ApplicationController
   end
 
   def next
-    ap cookies[:proposed_foods]
+    proposed_food_ids = cookies[:proposed_foods].split(',')
     nutrient = Nutrient.find(params[:nutrient_id])
-    new_food = Food.all.sample
-    render partial: 'food_cards', locals: { food: new_food, nutrient: nutrient }, formats: [:html]
+    food = nutrient.foods.order(measure_value: :desc).where.not(id: proposed_food_ids).first
+
+    proposed_food_ids << food.id
+    cookies[:proposed_foods] = proposed_food_ids.map(&:to_s).join(',')
+
+    render partial: 'food_cards', locals: { food: food, nutrient: nutrient }, formats: [:html]
   end
+
+
+  def filter_foods(new_food)
+    if cookies[:proposed_foods].include?(new_food.id)
+      # next
+    else
+      new_food
+    end
+  end
+
 end
