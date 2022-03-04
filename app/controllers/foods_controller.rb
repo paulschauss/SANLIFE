a020f265c6d79c7c8f3189e458ae4c60c9b2b51a
Paul Schauss <p+github@schauss.fr>
2022-03-04 16:38:50 +0100

    cool

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,16 +6,13 @@ class FoodsController < ApplicationController
   def next
     proposed_food_ids = cookies[:proposed_foods].split(',')
     nutrient = Nutrient.find(params[:nutrient_id])
-<<<<<<< HEAD
-    render partial: 'food_cards', locals: { food: Food.all.sample, nutrient: nutrient }, formats: [:html]
-=======
+
     food = nutrient.foods.order(measure_value: :desc).where.not(id: proposed_food_ids).first
 
     proposed_food_ids << food.id
     cookies[:proposed_foods] = proposed_food_ids.map(&:to_s).join(',')
 
     render partial: 'food_cards', locals: { food: food, nutrient: nutrient }, formats: [:html]
->>>>>>> b3b4f87c62e88b5a96f2bc0bf4af2c976f671b92
   end
 
 
