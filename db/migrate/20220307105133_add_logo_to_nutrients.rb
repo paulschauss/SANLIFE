ba63793943d58715890fd90bf2420116651ee416
Amine LAFI <aminelafi7@hotmail.com>
2022-03-07 11:55:13 +0100

    add logo

---
+++ db/migrate/20220307105133_add_logo_to_nutrients.rb
@@ -0,0 +1,4 @@
+class AddLogoToNutrients < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_103819) do
+ActiveRecord::Schema.define(version: 2022_03_07_105133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
