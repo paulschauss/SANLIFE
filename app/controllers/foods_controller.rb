c95ad2bfd8f663766316037f4a0376f36a18794d
Marie Delori <mariedelori@outlook.com>
2022-03-08 09:53:15 +0100

    filtered down to 4

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,16 +5,15 @@ class FoodsController < ApplicationController
 
   def next
     nutrient = Nutrient.find(params[:nutrient_id])
-    food = nutrient.foods.order(measure_value: :desc).where.not(id: proposed_food_ids).first
 
     proposed_food_ids = cookies[:proposed_foods].split(',')
+    food = nutrient.foods.order(measure_value: :desc).where.not(id: proposed_food_ids).first
     proposed_food_ids << food.id
     cookies[:proposed_foods] = proposed_food_ids.map(&:to_s).join(',')
     ap cookies[:proposed_foods]
     render partial: 'food_cards', locals: { food: food, nutrient: nutrient }, formats: [:html]
 
-    # before_food =
-
+    # before_food
     saved_foods_ids = cookies[:saved_foods].split(',')
     # saved_foods_ids.delete(???? LOOK AT LOGS)
     # saved_food_ids.push(food)
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,7 @@ class UserMailer < ApplicationMailer
     @foods = Food.find(params[:food_ids])
 
     @nutrients = @user.nutrients
+    @nutrients_with_food = @nutrients.map {|nutrient| [nutrient, nutrient.foods.order(measure_value: :desc).limit(4)] }.to_h
 
     mail(to: @user.email, subject: "Your Food Plan")
 
