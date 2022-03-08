6e379ba4f7d19e06dab51d84a4220b49679142f9
Paul Schauss <p+github@schauss.fr>
2022-03-08 14:35:13 +0100

    stringfy bug fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
   def stringify(impact)
-    impact.transform_keys! { |k| "<b>#{k}</b>"}
-    impact.transform_values! {|v| "(#{v}%)"}
+    impact.transform_keys! { |k| "<b>#{k}</b>" }
+    impact.transform_values! { |v| "(#{v}%)" }
     array = impact.keys.zip impact.values
-    sentence = array[0..-2].map{|a| a.join(" ")}.join(', ')
-    (sentence + " and " + array.last.flatten.join(' ')).html_safe
+    sentence = array[0..-2].map{ |a| a.join(" ") }.join(', ')
+    "#{sentence} and #{array.last.flatten.join(' ')}".html_safe
+    # (sentence + " and " + array.last.flatten.join(' ')).html_safe
   end
 end
