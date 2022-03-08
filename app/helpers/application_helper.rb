80a7163563a877d87af2e0971e5bf242ac7ab1ea
Paul Schauss <p+github@schauss.fr>
2022-03-08 10:19:28 +0100

    front 2 sur 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def stringify(impact)
+    impact.transform_keys! { |k| "<b>#{k}</b>"}
+    impact.transform_values! {|v| "(#{v}%)"}
+    array = impact.keys.zip impact.values
+    sentence = array[0..-2].map{|a| a.join(" ")}.join(', ')
+    (sentence + " and " + array.last.flatten.join(' ')).html_safe
+  end
 end
--- db/migrate/20220307143629_remove_string_from_nutrients.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStringFromNutrients < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :nutrients, :string, :string
-  end
-end
