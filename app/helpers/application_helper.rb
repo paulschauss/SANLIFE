542ffe8f29e1f41381b7390bb0de2501a999936b
Paul Schauss <p+github@schauss.fr>
2022-03-09 17:08:03 +0100

    ened

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-  def stringify(impact)
+  def stringify(illness_nutrients)
+
+    impact = {}
+    illness_nutrients.each do |illness_nutrient|
+      impact[illness_nutrient.nutrient.name] = illness_nutrient.impact # merci paul pour le non titleize.
+    end
+
     impact.transform_keys! { |k| "<b>#{k}</b>" }
     impact.transform_values! { |v| "<span class='pill'>#{v}%</span>" }
     array = impact.keys.zip impact.values
@@ -10,5 +16,15 @@ module ApplicationHelper
       sentence = array.map { |a| a.join(" ") }.join(', ')
       sentence.to_s.html_safe
     end
+  end
+
+  def render_svg(name, options = {})
+    options[:title] ||= name.underscore.humanize
+    options[:aria] = true
+    options[:nocomment] = true
+    options[:class] = options.fetch(:styles, "fill-current text-gray-500")
+
+    filename = "#{name}.svg"
+    inline_svg_tag(filename, options)
   end
 end
