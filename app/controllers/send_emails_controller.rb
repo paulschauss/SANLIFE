c2f288cc4e3813a352c8e36b47f60b0845e58d52
Marie Delori <mariedelori@outlook.com>
2022-03-10 20:18:10 +0100

    email and footer

--- app/controllers/send_emails_controller.rb
+++ app/controllers/send_emails_controller.rb
@@ -1,6 +1,6 @@
 class SendEmailsController < ApplicationController
   def nutrient_email
-    sleep(1)
+    sleep(1.5)
     UserMailer.with(user: current_user, excluded_food_ids: cookies[:excluded_foods].split(',').map(&:to_i))
               .email.deliver_now
 
--- app/javascript/controllers/email_controller.js
+++ app/javascript/controllers/email_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = [ 'button', 'loader', 'success' ]
+  static targets = [ 'button', 'loader', 'success', 'emailInput']
 
   showLoader() {
     this.buttonTarget.classList.add('d-none')
@@ -19,12 +19,15 @@ export default class extends Controller {
   send() {
     this.showLoader()
     fetch('/send_email', { method: 'POST' , headers: {
-      "X-CSRF-Token": csrfToken()
-    }})
+      "X-CSRF-Token": csrfToken(),
+      "Content-Type": "application/json"
+    }, body: JSON.stringify({ email: this.emailInputTarget.value })},
+    )
     .then(response => this.showSuccess())
   }
 
   connect() {
-    console.log("coucou")
+    console.log("email")
+    console.log(this.emailInputTarget)
   }
 }
