ed0752a7370b640a1a8077527477810ac7efc63e
Paul Schauss <p+github@schauss.fr>
2022-03-11 11:47:45 +0100

    hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    :password => ENV['SENGRID_APIKEY'], # This is the secret sendgrid API key which was issued during API key creation
+    :password => ENV['SENDGRID_APIKEY'], # This is the secret sendgrid API key which was issued during API key creation
     :domain => 'sanlife.me',
     :address => 'smtp.sendgrid.net',
     :port => 587,
